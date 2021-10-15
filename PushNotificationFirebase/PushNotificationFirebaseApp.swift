f2e6bc8fba1e962f09b871aff3f53b639f24c0db
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-10-15 16:48:37 +0700

    Create the project

---
+++ PushNotificationFirebase/PushNotificationFirebaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PushNotificationFirebaseApp.swift
+//  PushNotificationFirebase
+//
+//  Created by Danh Tu on 15/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct PushNotificationFirebaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
